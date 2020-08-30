7cdbae506a607192d410629c439e41edb4c6051f
Sandeep M.V.N <sandeep.mvn@deccansoft.com>
2020-08-30 17:21:11 +0530

    DI Example

--- DependencyInjectionExampleSolution/DepedencyInjectionUsingMVC/Controllers/HomeController.cs
+++ DependencyInjectionExampleSolution/DepedencyInjectionUsingMVC/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DepedencyInjectionUsingMVC.Models;
+using DepedencyInjectionUsingMVC.Service;
 
 namespace DepedencyInjectionUsingMVC.Controllers
 {
@@ -13,16 +14,49 @@ namespace DepedencyInjectionUsingMVC.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        //private readonly IMyDependency _dependencyService;
+
+        //controller-base Injection
+        //public HomeController(ILogger<HomeController> logger,IMyDependency myDependency)
+        //{
+        //    _logger = logger;
+        //    _dependencyService = myDependency;
+        //}
+
+
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+             _logger = logger;
         }
+        //using constructor injection
+        //public IActionResult Index()
+        //{
+        //    _dependencyService.WriteMessage("this from Index action method");
+        //    return View();
+        //}
 
+        ////method level injection
+        //public IActionResult Index([FromServices]IMyDependency dependencyService)
+        //{
+        //    dependencyService.WriteMessage("this from Index action method");
+        //    return View();
+        //}
+
+        //View level injection
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+        //without injection, accessing dependent services
         public IActionResult Index()
         {
+            var services = HttpContext.RequestServices;
+            var dependencyservice = (IMyDependency)services.GetService(typeof(IMyDependency));
+            dependencyservice.WriteMessage("without injection, accessing dependent services");
             return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
--- DependencyInjectionExampleSolution/DepedencyInjectionUsingMVC/Startup.cs
+++ DependencyInjectionExampleSolution/DepedencyInjectionUsingMVC/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DepedencyInjectionUsingMVC.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,10 @@ namespace DepedencyInjectionUsingMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+           // services.Add(new ServiceDescriptor(typeof(IMyDependency), typeof(LoggerDependencyService),ServiceLifetime.Singleton));
+
+            services.AddSingleton<IMyDependency, LoggerDependencyService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
